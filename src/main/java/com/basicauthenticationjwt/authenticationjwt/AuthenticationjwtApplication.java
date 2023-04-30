3c6843d8ad1266412febe4eeeb3b9f4d17f2b079
ivgeni <ivgeni@pop-os.localdomain>
2023-04-29 21:18:29 -0400

    first

---
+++ src/main/java/com/basicauthenticationjwt/authenticationjwt/AuthenticationjwtApplication.java
@@ -0,0 +1,13 @@
+package com.basicauthenticationjwt.authenticationjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationjwtApplication.class, args);
+	}
+
+}
